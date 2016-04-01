1472f7f3eba107728233115360f1e2698f52e8b7
Nicholas Bruning <nicholas@bruning.com.au>
2016-04-01 14:34:44 +1100

    Im a roo

---
+++ status/config.rb
@@ -0,0 +1,70 @@
+require 'active_support/all'
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+helpers do
+  def statcon(app_id)
+    app_events(app_id).select{ |event| event.closed_at.nil? }.any? ? 1 : 3
+  end
+
+  def statcon_in_words(statcon)
+    ['fail', 'warning', 'healthy'][statcon-1]
+  end
+
+  def app_events(app_id)
+    data.events.select do |event|
+      event.apps.include?(app_id)
+    end
+  end
+
+  def history(app_id, days: 90)
+    events = app_events(app_id)
+    days.times.map do |day|
+      start_time = (day.days.ago).at_midnight
+      end_time = (day.days.ago).at_end_of_day
+
+      day_events = events.select do |event|
+        created_at = event.created_at
+        closed_at = event.closed_at.present? && event.closed_at
+
+        if closed_at
+          (created_at >= start_time && created_at <= end_time) ||
+          (event.closed_at >= start_time && event.closed_at <= end_time)
+        else
+          start_time >= created_at
+        end
+      end
+      {
+        date: Time.now.at_midnight,
+        events: day_events,
+        statcon: day_events.any? ? 1 : 3
+      }
+    end
+  end
+end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+end
---
+++ status/lib/tasks/issues.rake
@@ -0,0 +1,30 @@
+require 'octokit'
+require 'yaml'
+require 'dotenv'
+require 'pry'
+
+namespace :issues do
+  desc 'Updates local data from GitHub issues'
+  task :update => :dotenv do
+    puts "Updating issues data from GitHub"
+    token  = ENV['GITHUB_TOKEN']
+    repos  = ['hooroo/hooroo-status']
+    client = Octokit::Client.new(access_token: token)
+
+    issues = Hash[repos.map do |repo|
+      print "."
+      STDOUT.flush
+      [repo, client.issues(repo).map { |i| i.to_h }]
+    end]
+
+    puts "\nDone"
+    puts "Writing events"
+
+    data_dir = File.join(File.dirname(__FILE__), '..', '..', 'data')
+    filename = File.join(data_dir, 'events.yml')
+    File.open(filename, 'w') do |fp|
+      fp.puts YAML.dump(issues[repos.first].map { |i| { id: i[:id], apps: i[:labels].map { |a| a[:name] }, created_at: i[:created_at], updated_at: i[:updated_at], closed_at: i[:closed_at] } })
+    end
+    puts "Done"
+  end
+end
